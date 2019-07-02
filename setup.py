252612fb0b9c5720aa09a691043e4c1402229d53
Melis <melis.zhoroev@gmail.com>
2019-07-03 03:03:52 +0700

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="rambler-kassa",
-    version="0.0.3",
+    version="0.0.4",
     description=('API Rambler Kassa', ),
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
